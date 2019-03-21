53c4fb3649e7fb0864fef9ffbbd6c79fabb65f32
Derek Houck <derek@derekhouck.com>
2019-03-21 08:46:28 -0700

    Add answer to answer screen

--- src/components/answer-screen.js
+++ src/components/answer-screen.js
@@ -1,39 +1,31 @@
 import React from 'react';
 import './answer-screen.css';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-export class AnswerScreen extends React.Component{
+export class AnswerScreen extends React.Component {
   onNext = (e) => {
     e.preventDefault();
-    console.log('onNext ran');
-    return this.props.history.push(`/dashboard`);
-    }
-  
+    return this.props.history.push('/dashboard');
+  }
 
-  render(){
-       if (this.props.correct === true){
-      return (
-        <div>
-          <h3>That's right</h3>
-          <button type="submit" className="next-button" onClick={(e) => this.onNext(e)}>Next</button>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <h3>Sorry, that's incorrect</h3>  
-          <button type="submit" className="next-button" onClick={(e) => this.onNext(e)}>Next</button>
-        </div>
-      )
-    }
+
+  render() {
+    return (
+      <div>
+        <h3>{this.props.correct ? 'That\'s right' : 'Sorry, that\'s incorrect'}</h3>
+        <img alt="traffic sign" src={this.props.sign} />
+        <p>The correct answer is <strong>{this.props.answer}</strong></p>
+        <button type="submit" className="next-button" onClick={(e) => this.onNext(e)}>Next</button>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-      sign: state.auth.currentSign,
-      answer: state.auth.answer,
-      correct: state.auth.correct
+    sign: state.auth.currentSign,
+    answer: state.auth.answer,
+    correct: state.auth.correct
   };
 };
 
