d62c5dfba652df11a626fbd43662d559749a51f3
Derek Houck <derek@derekhouck.com>
2019-03-22 07:51:19 -0700

    Add redirect on answers page when sign prop is undefined

--- src/components/answer-screen.js
+++ src/components/answer-screen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import './answer-screen.css';
 import { connect } from 'react-redux';
 
@@ -9,17 +10,21 @@ export class AnswerScreen extends React.Component {
   }
 
   render() {
-    return (
-      <section>
-        <h2>{this.props.sign.correct ? 'That\'s right' : 'Sorry, that\'s incorrect'}</h2>
-        <img alt={`A ${this.props.answer} sign`} src={this.props.sign.image} />
-        <p>The correct answer is <strong>{this.props.sign.answer}</strong></p>
-        <div>
-          <p>For this sign, you've made {this.props.sign.guessesMade} guesses, with {this.props.sign.guessesCorrect} of them being correct. You have gotten this sign correct {Math.floor(this.props.sign.guessesCorrect / this.props.sign.guessesMade * 100)}% of the time.</p>
-        </div>
-        <button className="next-button" onClick={(e) => this.onNext(e)}>Next</button>
-      </section>
-    );
+    if (this.props.sign === undefined) {
+      return <Redirect to="/dashboard" />;
+    } else {
+      return (
+        <section>
+          <h2>{this.props.sign.correct ? 'That\'s right' : 'Sorry, that\'s incorrect'}</h2>
+          <img alt={`A ${this.props.answer} sign`} src={this.props.sign.image} />
+          <p>The correct answer is <strong>{this.props.sign.answer}</strong></p>
+          <div>
+            <p>For this sign, you've made {this.props.sign.guessesMade} guesses, with {this.props.sign.guessesCorrect} of them being correct. You have gotten this sign correct {Math.floor(this.props.sign.guessesCorrect / this.props.sign.guessesMade * 100)}% of the time.</p>
+          </div>
+          <button className="next-button" onClick={(e) => this.onNext(e)}>Next</button>
+        </section>
+      );
+    }
   }
 }
 
