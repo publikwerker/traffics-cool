5c923e063cdef17aa863c990cfb53bd49a467de7
Derek Houck <derek@derekhouck.com>
2019-03-22 11:46:47 -0700

    Add styles to answer screen

--- src/components/answer-screen.js
+++ src/components/answer-screen.js
@@ -14,14 +14,14 @@ export class AnswerScreen extends React.Component {
       return <Redirect to="/dashboard" />;
     } else {
       return (
-        <section>
-          <h2>{this.props.sign.correct ? 'That\'s right' : 'Sorry, that\'s incorrect'}</h2>
-          <img alt={`A ${this.props.answer} sign`} src={this.props.sign.image} />
+        <section className={`answer ${this.props.sign.correct ? 'correct' : ''}`}>
+          <h2 className="answer-heading">{this.props.sign.correct ? 'That\'s right' : 'Sorry, that\'s incorrect'}</h2>
+          <img alt={`A ${this.props.answer} sign`} className="answer-image" src={this.props.sign.image} />
           <p>The correct answer is <strong>{this.props.sign.answer}</strong></p>
           <div>
             <p>For this sign, you've made {this.props.sign.guessesMade} guesses, with {this.props.sign.guessesCorrect} of them being correct. You have gotten this sign correct {Math.floor(this.props.sign.guessesCorrect / this.props.sign.guessesMade * 100)}% of the time.</p>
           </div>
-          <button className="next-button" onClick={(e) => this.onNext(e)}>Next</button>
+          <button className="btn btn-secondary next-button" onClick={(e) => this.onNext(e)}>Next</button>
         </section>
       );
     }
