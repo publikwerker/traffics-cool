598846addfdecb78da13c9b24b06ca03977a52e9
Publikwerker <jason.michael.hoffman@gmail.com>
2019-03-22 08:45:16 -0700

    display signs on progress page

--- src/components/progress-screen.js
+++ src/components/progress-screen.js
@@ -14,23 +14,32 @@ export class ProgressScreen extends React.Component {
       const { history } = this.props;
       return history.push('/dashboard');
   }
+
+  signList = (array) => {
+    console.log(array);
+    return array.map((sign, index) => {
+      return <img className="learned-signs" key={index} alt="traffic sign" src={sign}/>
+    })
+  }
   
   render(){
     let masteredList;
+    let percent = (this.props.guessesCorrect/this.props.guessesMade*100).toFixed(1);
     console.log(this.props.learned);
       if (isNullOrUndefined(this.props.learned)){
         masteredList = (<li>You haven't mastered any signs, yet.</li>);
       } else if (this.props.learned.length === 1){ 
         masteredList = (<li>You have mastered the {this.props.learned[0].sign} sign.</li>);
       } else if (this.props.learned.length > 1){ 
-        masteredList = (<li>Signs you have mastered: {this.props.learned}.</li>);
+        masteredList = (<li>Signs you have mastered: <br />{this.signList(this.props.learned)}</li>);
       }
     console.log(masteredList);
     return (
       <div>
         <ul className="progress-data">
           <li>Out of {this.props.guessesMade} questions,</li>
-          <li>you have gotten {this.props.guessesCorrect} correct.</li>
+          <li>You have gotten {this.props.guessesCorrect} correct.</li>
+          <li>That's {percent} percent right!</li>
           {masteredList}
           <button 
             type="submit"
